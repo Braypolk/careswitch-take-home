ecf40a60744bacfecfaf89e3859d4860ea888ea9
Bray Polkinghorne <braypolk@gmail.com>
2024-07-17 10:22:01 -0700

    sidebar, look nicer, collapse to side

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -1,56 +1,112 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 	import { Button } from '$lib/components/ui/button';
-	import { toggleMode } from 'mode-watcher';
 
-	import Sun from 'lucide-svelte/icons/sun';
-	import Moon from 'lucide-svelte/icons/moon';
+	import User from 'lucide-svelte/icons/user';
+	import CirclePlus from 'lucide-svelte/icons/circle-plus';
+	import LibraryBig from 'lucide-svelte/icons/library-big';
+	import ArrowRightToLine from 'lucide-svelte/icons/arrow-right-to-line';
+	import ArrowLeftToLine from 'lucide-svelte/icons/arrow-left-to-line';
+
+	let navActive = $state(false);
+	let keepOpen = $state(false);
 </script>
 
-<!-- --accent
-    --secondary
-    --primary
-    --card -->
-<nav class="h-screen w-64 bg-accent p-4 text-primary">
-	<div class="flex justify-between mb-5">
-		<h1 class="mb-4 text-2xl font-bold">Careswitch</h1>
-		<Button on:click={toggleMode} variant="outline" size="icon">
-			<Sun
-				class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+<nav
+	class="h-screen bg-accent p-4 text-primary"
+	on:mouseenter={() => (navActive = true)}
+	on:mouseleave={() => (navActive = keepOpen ? true : false)}
+>
+	{#if navActive}
+		<div class="flex">
+			<Button variant="ghost" class="mb-5 cursor-pointer text-2xl font-bold">Careswitch</Button>
+			<Button
+				on:click={() => (keepOpen = !keepOpen)}
+				variant="secondary"
+				class="w-10 p-0 hover:border hover:border-background"
+			>
+				{#if keepOpen}
+					<ArrowLeftToLine class="h-[1.2rem] w-[1.2rem]" />
+				{:else}
+					<ArrowRightToLine class="h-[1.2rem] w-[1.2rem]" />
+				{/if}
+			</Button>
+		</div>
+	{:else}
+		<svg
+			class="mx-auto mb-7 mt-2 h-6 w-6"
+			viewBox="0 0 378 381"
+			fill="none"
+			xmlns="http://www.w3.org/2000/svg"
+		>
+			<path
+				fill-rule="evenodd"
+				clip-rule="evenodd"
+				d="M378 190.5C378 295.71 292.71 381 187.5 381C95.479 381 18.6972 315.754 0.89231 229H74.1669C89.7774 274.943 133.278 308 184.5 308C248.841 308 301 255.841 301 191.5C301 127.159 248.841 75 184.5 75C133.651 75 90.4103 107.578 74.5118 153H0.68988C18.1079 65.7479 95.1258 0 187.5 0C292.71 0 378 85.2898 378 190.5Z"
+				class="fill-primary"
 			/>
-			<Moon
-				class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
-			/>
-			<span class="sr-only">Toggle theme</span>
-		</Button>
-	</div>
+		</svg>
+	{/if}
 	<ul class="space-y-2">
 		<li>
-			<Button
-				variant={$page.url.pathname === '/users' ? 'default' : 'ghost'}
-				class="w-full justify-start"
-				href="/users"
-			>
-				Users
-			</Button>
+			{#if navActive}
+				<Button
+					variant={$page.url.pathname === '/users' ? 'default' : 'ghost'}
+					class="w-full justify-start"
+					href="/users"
+				>
+					<User class="mr-2" />
+					Users
+				</Button>
+			{:else}
+				<Button
+					variant={$page.url.pathname === '/users' ? 'default' : 'ghost'}
+					class="justify-start px-2"
+					href="/users"
+				>
+					<User class="mr" />
+				</Button>
+			{/if}
 		</li>
 		<li>
-			<Button
-				variant={$page.url.pathname === '/workspaces' ? 'default' : 'ghost'}
-				class="w-full justify-start"
-				href="/workspaces"
-			>
-				Workspaces
-			</Button>
+			{#if navActive}
+				<Button
+					variant={$page.url.pathname === '/workspaces' ? 'default' : 'ghost'}
+					class="w-full justify-start"
+					href="/workspaces"
+				>
+					<LibraryBig class="mr-2" />
+					Workspaces
+				</Button>
+			{:else}
+				<Button
+					variant={$page.url.pathname === '/workspaces' ? 'default' : 'ghost'}
+					class="justify-start px-2"
+					href="/workspaces"
+				>
+					<LibraryBig class="mr" />
+				</Button>
+			{/if}
 		</li>
 		<li>
-			<Button
-				variant={$page.url.pathname === '/create' ? 'default' : 'ghost'}
-				class="w-full justify-start"
-				href="/create"
-			>
-				Create
-			</Button>
+			{#if navActive}
+				<Button
+					variant={$page.url.pathname === '/create' ? 'default' : 'ghost'}
+					class="w-full justify-start"
+					href="/create"
+				>
+					<CirclePlus class="mr-2" />
+					Create
+				</Button>
+			{:else}
+				<Button
+					variant={$page.url.pathname === '/create' ? 'default' : 'ghost'}
+					class="justify-start px-2"
+					href="/create"
+				>
+					<CirclePlus class="mr" />
+				</Button>
+			{/if}
 		</li>
 	</ul>
 </nav>
